487ba18452c8bc3249705e3e68a7829c54b95e71
Timothé <timothe.lorin@gmail.com>
2025-12-18 12:16:44 +0100

    [ADD] Pickups spawn points

---
+++ Source/Overfront/Private/Pickups/OFPickupSpawnPoint.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickups/OFPickupSpawnPoint.h"
+
+#include "Pickups/OFPickup.h"
+
+AOFPickupSpawnPoint::AOFPickupSpawnPoint()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+}
+
+void AOFPickupSpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	StartSpawnPickupTimer();
+}
+
+void AOFPickupSpawnPoint::SpawnPickup()
+{
+	if (PickupClasses.Num() > 0)
+	{
+		int32 RandomIndex = FMath::RandRange(0, PickupClasses.Num() - 1);
+		TSubclassOf<AOFPickup> RandomPickupClass = PickupClasses[RandomIndex];
+		
+		SpawnedPickup = GetWorld()->SpawnActor<AOFPickup>(RandomPickupClass, GetActorTransform());
+		if (HasAuthority() && SpawnedPickup)
+		{
+			SpawnedPickup->OnDestroyed.AddDynamic(this, &AOFPickupSpawnPoint::OnPickupDestroyed); 
+		}
+		
+	}
+}
+
+void AOFPickupSpawnPoint::StartSpawnPickupTimer()
+{
+	const float SpawnWaitingDuration = FMath::FRandRange(MinSpawnDuration, MaxSpawnDuration);
+	GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, this, &AOFPickupSpawnPoint::SpawnPickupTimerFinished, SpawnWaitingDuration);
+}
+
+void AOFPickupSpawnPoint::SpawnPickupTimerFinished()
+{
+	if (HasAuthority())
+	{
+		SpawnPickup();
+	}
+}
+
+void AOFPickupSpawnPoint::OnPickupDestroyed(AActor* DestroyedActor)
+{
+	SpawnedPickup = nullptr;
+	StartSpawnPickupTimer();
+}
+
+
---
+++ Source/Overfront/Public/Pickups/OFPickupSpawnPoint.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "OFPickupSpawnPoint.generated.h"
+
+
+UCLASS()
+class OVERFRONT_API AOFPickupSpawnPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AOFPickupSpawnPoint();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	TArray<TSubclassOf<class AOFPickup>> PickupClasses;
+	
+	void SpawnPickup();
+private:
+	FTimerHandle SpawnTimerHandle;
+	void StartSpawnPickupTimer();
+	void SpawnPickupTimerFinished();
+	
+	UPROPERTY(EditAnywhere)
+	float MinSpawnDuration = 10.f;
+	
+	UPROPERTY(EditAnywhere)
+	float MaxSpawnDuration = 30.f;
+	
+	UPROPERTY()
+	AOFPickup* SpawnedPickup;
+	
+	UFUNCTION()
+	void OnPickupDestroyed(AActor* DestroyedActor);
+};
